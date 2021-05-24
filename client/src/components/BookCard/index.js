fcd85b97998f111ce0e024c0ac141b1ffcf163bd
myles502 <80232038+myles502@users.noreply.github.com>
2021-05-23 22:27:58 -0700

    basic layout

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,4 +1,21 @@
 function BookCard(){
-    return <div>BookCard</div>
+    return <div classname="card mb-3">
+  <div classname="row no gutters">
+    <div classname="col-md-4">
+      <img src="..." alt="..." />
+    </div>
+    <div classname="col-md-8">
+      <div classname="card-body">
+        <h5 classname="card-title">Card Title</h5>
+        <p classname="card-text">This is sample card text</p>
+       <div>
+  <a href="#" className="btn btn-primary mr-3">View</a>
+  <button class="btn btn-secondary ">Save</button>
+</div>
+
+      </div>
+    </div>
+  </div>BookCard</div>
+
 }
 export default BookCard;
